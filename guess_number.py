d0198387a3ff31f3ae4080bc0252291974de212b
Skuratova Evgeniya <evgeniaskuratova@ya.ru>
2024-04-03 23:00:06 +0400

    last commit

--- guess_number.py
+++ guess_number.py
@@ -2,14 +2,14 @@ from random import randint
 
 
 number = randint(1, 100)
-print('Угадайте число от 1 до 100')
+print("Угадайте число от 1 до 100")
 
 while True:
-    guess = int(input('Введите число: '))
+    guess = int(input("Введите число: "))
     if guess < number:
-        print('Ваше число меньше того, что загадано')
+        print("Ваше число меньше того, что загадано")
     elif guess > number:
-        print('Ваше число больше того, что загадано')
+        print("Ваше число больше того, что загадано")
     elif guess == number:
         break
-print('Отличная интуиция! Вы угадали число :)')
+print("Отличная интуиция! Вы угадали число :)")
